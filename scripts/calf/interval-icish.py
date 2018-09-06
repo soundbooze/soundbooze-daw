545d6db5d85773a137843339b33d13ac1026e312
bruno/br <soundbooze@gmail.com>
2018-09-06 20:08:55 +0700

    Add files via upload

--- scripts/calf/interval-icish.py
+++ scripts/calf/interval-icish.py
@@ -29,26 +29,49 @@ def ConnectJack():
     PLAYBACK2 = 'system:playback_2'
     PULSEAUDIO1 = 'PulseAudio JACK Sink:front-left'
     PULSEAUDIO2 = 'PulseAudio JACK Sink:front-right'
+    HYDROGEN1 = 'Hydrogen:out_L'
+    HYDROGEN2 = 'Hydrogen:out_R'
+
+    IN1 = "In #1"
+    IN2 = "In #2"
+    OUT1 = "Out #1"
+    OUT2 = "Out #2"
+    CALF_ANALYZER = "Calf Studio Gear:Analyzer"
+    CALF_8BAND_EQUALIZER = "Calf Studio Gear:Equalizer 8 Band"
+    CALF_MULTIBAND_COMPRESSOR = "Calf Studio Gear:Multiband Compressor"
+    CALF_MULTIBAND_LIMITER = "Calf Studio Gear:Multiband Limiter"
+    CALF_REVERB = "Calf Studio Gear:Reverb"
 
     client = jack.Client('interval-icish')
 
-    regexPort = client.get_ports('(Calf Studio Gear:Analyzer|Calf Studio Gear:Equalizer 8 Band)')
+    regexPort = client.get_ports('(Calf Studio Gear:Analyzer|Calf Studio Gear:Equalizer 8 Band|Calf Studio Gear:Multiband Compressor|Calf Studio Gear:Multiband Limiter)')
     if (regexPort):
 
         try:
             if (sys.argv[1] == "playback"):
-                client.connect(MONITOR1, 'Calf Studio Gear:Analyzer In #1')
-                client.connect(MONITOR2, 'Calf Studio Gear:Analyzer In #2')
+                client.connect(MONITOR1, CALF_ANALYZER + " " + IN1)
+                client.connect(MONITOR2, CALF_ANALYZER + " " + IN2)
             elif (sys.argv[1] == "capture"):
-                client.connect(CAPTURE1, 'Calf Studio Gear:Analyzer In #1')
-                client.connect(CAPTURE2, 'Calf Studio Gear:Analyzer In #2')
+                client.connect(CAPTURE1, CALF_ANALYZER + " " + IN1)
+                client.connect(CAPTURE2, CALF_ANALYZER + " " + IN2)
             elif (sys.argv[1] == "pulseaudio"):
                 client.disconnect(PULSEAUDIO1, PLAYBACK1)
                 client.disconnect(PULSEAUDIO2, PLAYBACK2)
-                client.connect(PULSEAUDIO1, 'Calf Studio Gear:Equalizer 8 Band In #1')
-                client.connect(PULSEAUDIO2, 'Calf Studio Gear:Equalizer 8 Band In #2')
-                client.connect('Calf Studio Gear:Equalizer 8 Band Out #1', PLAYBACK1)
-                client.connect('Calf Studio Gear:Equalizer 8 Band Out #2', PLAYBACK2)
+                client.connect(PULSEAUDIO1, CALF_8BAND_EQUALIZER + " " + IN1)
+                client.connect(PULSEAUDIO2, CALF_8BAND_EQUALIZER + " " + IN2)
+                client.connect(CALF_8BAND_EQUALIZER + " " + OUT1, PLAYBACK1)
+                client.connect(CALF_8BAND_EQUALIZER + " " + OUT2, PLAYBACK2)
+            elif (sys.argv[1] == "hydrogen"):
+                client.disconnect(HYDROGEN1, PLAYBACK1)
+                client.disconnect(HYDROGEN2, PLAYBACK2)
+                client.connect(HYDROGEN1, CALF_MULTIBAND_COMPRESSOR + " " + IN1)
+                client.connect(HYDROGEN2, CALF_MULTIBAND_COMPRESSOR + " " + IN2)
+                client.connect(CALF_MULTIBAND_COMPRESSOR + " " + OUT1, CALF_MULTIBAND_LIMITER + " " + IN1)
+                client.connect(CALF_MULTIBAND_COMPRESSOR + " " + OUT2, CALF_MULTIBAND_LIMITER + " " + IN2)
+                client.connect(CALF_MULTIBAND_LIMITER + " " + OUT1, CALF_REVERB + " " + IN1)
+                client.connect(CALF_MULTIBAND_LIMITER + " " + OUT2, CALF_REVERB + " " + IN2)
+                client.connect(CALF_REVERB + " " + OUT1, PLAYBACK1)
+                client.connect(CALF_REVERB + " " + OUT2, PLAYBACK2)
 
         except:
             pass
