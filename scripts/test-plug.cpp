64285ef15e65021e3fbd74aebd36b794750e9fec
bruno/br <soundbooze@gmail.com>
2018-09-11 11:25:17 +0700

    Add files via upload

--- scripts/test-plug.cpp
+++ scripts/test-plug.cpp
@@ -3,6 +3,7 @@
  * amps setting
  * effects
  *
+ * g++ test-plug.cpp -o test-plug `pkg-config --libs --cflags libusb-1.0` mustang.o
  * 2s sample
  *
  */
@@ -22,7 +23,7 @@ main(int argc, char *argv[]) {
     amps ampStart = FENDER_57_DELUXE;
     amps ampEnd = METAL_2000;
 
-    memset(names, 0x00, 100*32);
+    memset(names, 0x00, 100 * 32);
 
     Mustang mustang;
 
@@ -33,31 +34,26 @@ main(int argc, char *argv[]) {
       printf("%s\n", names[i]);
     */
 
-    printf ("%d %d %d %d %d %d\n", amplifier_set.gain, amplifier_set.volume, 
-                                   amplifier_set.treble, amplifier_set.middle, 
-                                   amplifier_set.bass, amplifier_set.master_vol);
-
     int slot = 0;
 
-    //for (i = 0; i < 25; i++) {
     while (1) {
-      /*
+
       mustang.load_memory_bank(i++, name, &amplifier_set, effects_set);
       if (i >= 25)
         i = 0;
-        */
 
-      printf ("%d %d %d %d %d %d\n", amplifier_set.gain, amplifier_set.volume, 
-                   amplifier_set.treble, amplifier_set.middle, 
-                   amplifier_set.bass, amplifier_set.master_vol);
+      printf("[amp_num : %d ] [gain : %d] [volume : %d] [treble : %d] [middle : %d] [bass : %d] [cabinet : %d] [noise_gate : %d] [master_vol : %d] \n[gain2 : %d] [presence : %d] [threshold : %d] [depth : %d] [bias : %d] [sag : %d] [brightness : %d] [usb_gain : %d]\n\n", amplifier_set.amp_num, amplifier_set.gain, amplifier_set.volume, amplifier_set.treble, amplifier_set.middle, amplifier_set.bass, amplifier_set.cabinet, amplifier_set.noise_gate, amplifier_set.master_vol, amplifier_set.gain2, amplifier_set.presence, amplifier_set.threshold, amplifier_set.depth, amplifier_set.bias, amplifier_set.sag, amplifier_set.brightness, amplifier_set.usb_gain); 
 
+      /*
       amplifier_set.gain += 10;
       amplifier_set.volume += 10;
       amplifier_set.master_vol += 10;
 
       mustang.set_amplifier(amplifier_set);
+      */
 
-      sleep(2);
+      sleep(1);
+
     }
 
     mustang.stop_amp();
