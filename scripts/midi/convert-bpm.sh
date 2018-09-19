b3356b0e2a71efe2a347f41200f08cd3808a0a32
bruno/br <soundbooze@gmail.com>
2018-09-19 13:29:07 +0700

    Update convert-bpm.sh

--- scripts/midi/convert-bpm.sh
+++ scripts/midi/convert-bpm.sh
@@ -2,7 +2,7 @@
 
 midicsv $1 out.csv
 QN=`echo "60000/$2" | bc`
-TEMPO=`echo "$QN * 1000" | bc`
+TEMPO=`echo "$QN * 1000" | bc` # Tempo is xxx.xxx microseconds per beat
 sed -i -e 's/Tempo, [0-9]*/Tempo, '$TEMPO'/g' out.csv
 csvmidi out.csv new.mid
 rm -f out.csv
